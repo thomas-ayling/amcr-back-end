0f36915c58dec5450fed357ce74558d882493a1c
thomas-ayling <thomas.ayling@ecs.co.uk>
2023-01-07 11:55:03 +0000

    Added not nullable fields to the schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,7 +2,7 @@ create table if not exists feedback
 (
     id             uuid primary key,
     feedback_order serial,
-    feedback_type  text,
+    feedback_type  text not null,
     first_name     text,
     last_name      text,
     email_address  text,
@@ -14,22 +14,66 @@ create table if not exists feedback
 create table if not exists files
 (
     id           uuid primary key,
-    file_name    text,
-    file_type    text,
-    file_size    bigint,
-    data         bytea,
-    download_uri text,
+    file_name    text   not null,
+    file_type    text   not null,
+    file_size    bigint not null,
+    data         bytea  not null,
+    download_uri text   not null,
     feedback_id  uuid references feedback (id) on delete cascade
 );
 
 create table if not exists case_studies
 (
-    id               uuid primary key,
-    spotlight        boolean,
-    title            text,
-    overview         text,
-    cover_image_link text,
-    body             jsonb,
-    pdf_link         text,
-    pptx_link        text
-)
+    id               uuid primary key not null,
+    spotlight        boolean          not null,
+    title            text             not null,
+    overview         text             not null,
+    cover_image_link text             not null,
+    body             jsonb            not null,
+    download_links   text[]
+);
+
+-- {
+--     "spotlight": true,
+--     "title": "This is the title - 8 parts ",
+--     "overview": "This is the overview",
+--     "coverImageLink": "This is a link :/",
+--     "body": {
+--         "content": [
+--             {
+--                 "imageLink": "https://picsum.photos/500/300",
+--                 "markdownText": "A paragraph with *emphasis* and **strong importance**. > A block quote with ~strikethrough~ and a URL: https://reactjs.org. \n * Lists \n * [ ] todo \n * [x] done \n A table:\n \n | a | b |\n | - | - |"
+--             },
+--             {
+--                 "imageLink": "https://picsum.photos/500/300",
+--                 "markdownText": "## This is the header of the block \n This is even more text, just doing it for fun as you do ~~hello~~ \n #### subheading3 \n - bullet "
+--             },
+--             {
+--                 "imageLink": "https://picsum.photos/500/300",
+--                 "markdownText": "### This is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template,\n - this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, \n * [x] this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template"
+--             },
+--             {
+--                 "imageLink": "https://picsum.photos/500/300",
+--                 "markdownText": "This is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template"
+--             },
+--             {
+--                 "imageLink": "https://picsum.photos/500/300",
+--                 "markdownText": "## This is the header of the block \n This is even more text, just doing it for fun as you do ~~hello~~ \n #### subheading3 \n - bullet "
+--             },
+--             {
+--                 "imageLink": "https://picsum.photos/500/300",
+--                 "markdownText": "### This is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template,\n - this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, \n * [x] this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template"
+--             },
+--             {
+--                 "imageLink": "https://picsum.photos/500/300",
+--                 "markdownText": "## This is the header of the block \n This is even more text, just doing it for fun as you do ~~hello~~ \n #### subheading3 \n - bullet "
+--             },
+--             {
+--                 "imageLink": "https://picsum.photos/500/300",
+--                 "markdownText": "### This is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template,\n - this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, \n * [x] this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template, this is a lot of markdown text to be used as a template"
+--             }
+--         ]
+--     },
+--         "downloadLinks": ["This is a PDF link", "This is a PPTX link"]
+--     }
+-- }
