57737050c81aef436a5cacbd330b90e93ad0f1d5
thomas-ayling <thomas.ayling@ecs.co.uk>
2023-01-25 09:55:53 +0000

    Revert "Delete Attachment.java"
    This reverts commit 86017e7de3f2c047b7833e267f3b3214f6be7e33.

---
+++ src/main/java/com/globallogic/amcr/model/attachmentcomponent/Attachment.java
@@ -0,0 +1,38 @@
+package com.globallogic.amcr.model.attachmentcomponent;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class Attachment extends AttachmentMetadata {
+    private byte[] content;
+
+    public Attachment(UUID id, String name, long size, String type, long crc, Map<String, Object> metadata, byte[] content, String downloadUri) {
+        super(id, name, size, type, crc, metadata, downloadUri);
+        this.content = content;
+    }
+
+    public Attachment() {
+        super();
+    }
+
+    public static Attachment from(Attachment attachment, byte[] content) {
+        Attachment newAttachment = new Attachment();
+        newAttachment.setId(attachment.getId());
+        newAttachment.setName(attachment.getName());
+        newAttachment.setSize(attachment.getSize());
+        newAttachment.setType(attachment.getType());
+        newAttachment.setCrc(attachment.getCrc());
+        newAttachment.setMetadata(attachment.getMetadata());
+        newAttachment.setDownloadUri(attachment.getDownloadUri());
+        newAttachment.content = content;
+        return newAttachment;
+    }
+
+    public byte[] getContent() {
+        return content;
+    }
+
+    public void setContent(byte[] content) {
+        this.content = content;
+    }
+}
