63f7f0ff2f71c8e6c21ddf41843e64aaf8b64621
thomas-ayling <thomas.ayling@ecs.co.uk>
2023-01-25 16:14:06 +0000

    merge

---
+++ src/main/java/com/globallogic/amcr/repository/attachmentcomponent/AttachmentDao.java
@@ -0,0 +1,25 @@
+package com.globallogic.amcr.repository.attachmentcomponent;
+
+import com.globallogic.amcr.model.attachmentcomponent.Attachment;
+import com.globallogic.amcr.model.attachmentcomponent.AttachmentMetadata;
+import com.globallogic.amcr.model.attachmentcomponent.AttachmentResponse;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface AttachmentDao {
+
+    byte[] getBinary(UUID id);
+
+    Attachment update(UUID id, byte[] content, Attachment oldAttachment);
+
+    void delete(UUID id);
+
+    Attachment save(Attachment attachment, UUID id);
+
+    Attachment get(UUID id);
+
+    List<AttachmentResponse> getAll();
+
+    AttachmentMetadata getMetadata(UUID id);
+}
