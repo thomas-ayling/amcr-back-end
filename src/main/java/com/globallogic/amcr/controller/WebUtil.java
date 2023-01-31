17441aa82ef857331e69f3236dac367a67fef374
sourcesmith <14824422+sourcesmith@users.noreply.github.com>
2023-01-31 09:56:10 +0000

    Static library classes should be final.

--- src/main/java/com/globallogic/amcr/controller/WebUtil.java
+++ src/main/java/com/globallogic/amcr/controller/WebUtil.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.UUID;
 
-public class WebUtil {
+public final class WebUtil {
     private WebUtil () {
         throw new UnsupportedOperationException("WebUtil cannot be instantiated this way and its methods should be called statically");
     }
