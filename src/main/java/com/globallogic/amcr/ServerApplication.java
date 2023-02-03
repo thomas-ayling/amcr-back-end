42e05ddc2065479ced25e37767138a2504c591e5
CSCairney-GL <119429189+CSCairney-GL@users.noreply.github.com>
2023-02-03 10:47:23 +0000

    Removed excess space in Application.

--- src/main/java/com/globallogic/amcr/ServerApplication.java
+++ src/main/java/com/globallogic/amcr/ServerApplication.java
@@ -3,7 +3,6 @@ package com.globallogic.amcr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class ServerApplication {
     public static void main(String[] args) {
